0087af9694cb76718199ddfa43f9511cb95c7e06
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-09-07 09:18:56 -0500

    remove keys

--- app/helpers/sport_api.rb
+++ app/helpers/sport_api.rb
@@ -1,12 +1,12 @@
 module SportAPI
   def self.team_call
-    teams_response = HTTParty.get("https://api.sportradar.us/mlb-t5/league/active_rosters.json?api_key=88wqfh5nswegkgjhp23ptrse")
+    teams_response = HTTParty.get("https://api.sportradar.us/mlb-t5/league/active_rosters.json?api_key=KEY_HERE")
     active_teams_hash = teams_response.parsed_response
     teams = active_teams_hash['teams']
   end
 
   def self.player_call(api_id)
-    player_response = HTTParty.get("https://api.sportradar.us/mlb-t5/players/#{api_id}/profile.json?api_key=88wqfh5nswegkgjhp23ptrse")
+    player_response = HTTParty.get("https://api.sportradar.us/mlb-t5/players/#{api_id}/profile.json?api_key=KEY_HERE")
     player_JSON = player_response.parsed_response
     player_JSON['player']
   end
